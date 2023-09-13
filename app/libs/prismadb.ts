418ff2dd598757fe3840bdbdc823c9e307345e9d
daichitakahashi <t.daichi.snd@gmail.com>
2023-09-13 14:43:33 +0900

    NextAuth setup

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = (() => {
+  if (process.env.NODE_ENV !== "production") {
+    return new PrismaClient();
+  }
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  return global.prisma;
+})();
+
+export default prisma;
